b1c41a1ebe33f48f330e4feaa3c28ba9be9ef24a
Zaid Dar <63739210+validity-check@users.noreply.github.com>
2021-10-30 17:23:50 +0100

    Improve string similarity algorithm

--- ShopCMDLine/Main.java
+++ ShopCMDLine/Main.java
@@ -23,7 +23,7 @@ public class Main {
         shop.add(new ShopItem("Hat", random.nextInt(8), random.nextInt(500)));
         shop.add(new ShopItem("Bag", random.nextInt(30), random.nextInt(90)));
         shop.add(new ShopItem("Jacket", random.nextInt(15), random.nextInt(20)));
-        shop.add(new ShopItem("Kew Gardens 50p Brilliant Uncirculated Coin", random.nextInt(500), 1));
+        shop.add(new ShopItem("Kew Gardens 50p", random.nextInt(500), 1));
     }
 
     public static void shopLoop(ArrayList<ShopItem> shop) {
@@ -83,59 +83,79 @@ public class Main {
         System.out.println("Please enter the name of the product you want to select:\n> ");
         String selection = sc.next();
         int closestName = 0;
-        double mostSimilar = 0.0;
+        double mostSimilar = 0;
         for (int i = 0; i < shop.size(); i++) {
-            int matchingChar = matchingChar(selection.toLowerCase(), shop.get(i).getName().toLowerCase().toCharArray());
+            String currItem = shop.get(i).getName().toLowerCase();
+            String longer = currItem, shorter = selection;
+            if (selection.length() > currItem.length()) {
+                longer = selection;
+                shorter = currItem;
+            }
+            double matchingChar = (longer.length() - editDistance(longer, shorter)) / (double) longer.length();
             if (matchingChar > mostSimilar) {
                 mostSimilar = matchingChar;
                 closestName = i;
             }
         }
-        System.out.println("Did you mean " + shop.get(closestName).getName() + "?");
+        System.out.println("Did you mean " + shop.get(closestName).getName() + "?" + mostSimilar + " " + closestName);
         System.out.println("Y to confirm, N for no");
         String confirmation = "";
-        // while (confirmation != "y" && confirmation != "n") {
-        confirmation = sc.next().toLowerCase();
-        if (confirmation != "y" && confirmation != "n") {
-            System.out.println("Sorry, that input was not recognised");
-            System.out.println("Y to confirm, N for no");
-        }
-        // }
-        if (confirmation == "y") {
-            System.out.println("Product details:");
-            System.out.println("----------------------------------");
-            System.out.println("ITEM " + closestName);
-            System.out.println("NAME: " + shop.get(closestName).getName());
-            System.out.println("PRICE: £" + shop.get(closestName).getPrice());
-            System.out.println("STOCK: " + shop.get(closestName).getStock());
-            System.out.println("----------------------------------");
-            System.out.println("What would you like to do with this product?");
-            System.out.println("1 - Buy product.");
-            System.out.println("2 - Stock up on product.");
-            System.out.println("3 - Go back");
-            int choice = receiveInput(sc, 1, 3);
-            if (choice == 1) {
-                System.out.println("How many do you want?");
-                int qty = receiveInput(sc, 1, shop.get(closestName).getStock());
-                shop.get(closestName).stockDown(qty);
+        boolean yn = true;
+        while (yn) {
+            confirmation = sc.nextLine().toLowerCase();
+            switch (confirmation) {
+            case "y":
+                System.out.println("Product details:");
+                System.out.println("----------------------------------");
+                System.out.println("ITEM " + closestName);
+                System.out.println("NAME: " + shop.get(closestName).getName());
+                System.out.println("PRICE: £" + shop.get(closestName).getPrice());
+                System.out.println("STOCK: " + shop.get(closestName).getStock());
+                System.out.println("----------------------------------");
+                System.out.println("What would you like to do with this product?");
+                System.out.println("1 - Buy product.");
+                System.out.println("2 - Stock up on product.");
+                System.out.println("3 - Go back");
+                int choice = receiveInput(sc, 1, 3);
+                if (choice == 1) {
+                    if (shop.get(closestName).getStock() == 0) {
+                        System.out.println("Sorry, this item is out of stock");
+                        return;
+                    }
+                    System.out.println("How many do you want?");
+                    int qty = receiveInput(sc, 1, shop.get(closestName).getStock());
+                    shop.get(closestName).stockDown(qty);
+
+                }
+            case "n":
+                yn = false;
             }
         }
     }
 
-    private static int matchingChar(String s, char[] ch) {
+    public static int editDistance(String s1, String s2) {
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
 
-        int freq = 0, c = ch.length;
-
-        // Traverse the character array
-        for (int i = 0; i < c; i++) {
-
-            // If character matches
-            // then increment the count
-            if (s.contains(String.valueOf(ch[i]))) {
-                freq++;
+        int[] costs = new int[s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            int lastValue = i;
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0)
+                    costs[j] = j;
+                else {
+                    if (j > 0) {
+                        int newValue = costs[j - 1];
+                        if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                        costs[j - 1] = lastValue;
+                        lastValue = newValue;
+                    }
+                }
             }
+            if (i > 0)
+                costs[s2.length()] = lastValue;
         }
-
-        return freq;
+        return costs[s2.length()];
     }
 }
