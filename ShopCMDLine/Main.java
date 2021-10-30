189322c6ef2f25be428ea3ec562548080c0c064a
Zaid Dar <63739210+validity-check@users.noreply.github.com>
2021-10-30 15:32:42 +0100

    Far into select product, function 2

--- ShopCMDLine/Main.java
+++ ShopCMDLine/Main.java
@@ -27,8 +27,8 @@ public class Main {
     }
 
     public static void shopLoop(ArrayList<ShopItem> shop) {
+        Scanner sc = new Scanner(System.in);
         while (true) {
-            Scanner sc = new Scanner(System.in);
             System.out.println("|--------------------------------|");
             System.out.println("Select your option");
             System.out.println("|--------------------------------|");
@@ -38,6 +38,9 @@ public class Main {
             int choice = receiveInput(sc);
             if (choice == 1) {
                 printShop(shop);
+            }
+            if (choice == 2) {
+                selectProduct(shop, sc);
             }
             if (choice == 3) {
                 System.out.println("|--------------------------------|");
@@ -75,16 +78,60 @@ public class Main {
         }
     }
 
-    public static void selectProduct(ArrayList<ShopItem> shop) {
-        Scanner sc = new Scanner(System.in);
+    public static void selectProduct(ArrayList<ShopItem> shop, Scanner sc) {
         System.out.println("----------------------------------");
         System.out.println("Please enter the name of the product you want to select:\n> ");
         String selection = sc.next();
-        String closestName;
-        int mostSimilar;
+        int closestName = 0;
+        double mostSimilar = 0.0;
         for (int i = 0; i < shop.size(); i++) {
-
+            int matchingChar = matchingChar(selection.toLowerCase(), shop.get(i).getName().toLowerCase().toCharArray());
+            if (matchingChar > mostSimilar) {
+                mostSimilar = matchingChar;
+                closestName = i;
+            }
         }
-        sc.close();
+        if (0 <= closestName & closestName >= 3) {
+            System.out.println("Did you mean " + shop.get(closestName).getName() + "?");
+            System.out.println("Y to confirm, N for no");
+            String confirmation = "";
+            while (confirmation != "y" & confirmation != "n") {
+                confirmation = sc.next();
+                if (confirmation != "y" && confirmation != "n") {
+                    System.out.println("Sorry, that input was not recognised");
+                    System.out.println("Y to confirm, N for no");
+                }
+            }
+            if (confirmation == "y") {
+                System.out.println("Product details:");
+                System.out.println("----------------------------------");
+                System.out.println("ITEM " + closestName);
+                System.out.println("NAME: " + shop.get(closestName).getName());
+                System.out.println("PRICE: £" + shop.get(closestName).getPrice());
+                System.out.println("STOCK: " + shop.get(closestName).getStock());
+                System.out.println("----------------------------------");
+                System.out.println("What would you like to do with this product?");
+                System.out.println("1 - Buy product.");
+                System.out.println("2 - Stock up on product.");
+                System.out.println("3 - Go back");
+            }
+        }
+    }
+
+    private static int matchingChar(String s, char[] ch) {
+
+        int freq = 0, c = ch.length;
+
+        // Traverse the character array
+        for (int i = 0; i < c; i++) {
+
+            // If character matches
+            // then increment the count
+            if (s.contains(String.valueOf(ch[i]))) {
+                freq++;
+            }
+        }
+
+        return freq;
     }
 }
